2e4ab99373999e4512ab7d6e35bcf27476cd0ec0
Dominic <dominicezeifejafor@gmail.com>
2023-09-04 10:58:32 +0100

    Scroll To Top

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/First'
 import Second from './pages/Second'
 import Third from './pages/Third'
 import Nav from './components/Nav/Nav'
+import ScrollToTop from './components/Scroll/ScrollToTop';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/second' element={<Second />} />
         <Route path='/third' element={<Third />} />
       </Routes>
+      <ScrollToTop />
     </>
   );
 }
---
+++ src/components/Scroll/ScrollToTop.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+import './ScrollToTop.css'
+
+const ScrollToTop = () => {
+    const [backToTop, setBackToTop] = useState(false)
+    useEffect(() => {
+      window.addEventListener('scroll', () => {
+        if (window.scrollY > 100) {
+          setBackToTop(true)
+        } else {
+          setBackToTop(false)
+        }
+      })
+    }, [])
+
+    const scrollUp = () => {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      })
+    }
+    console.log('Scrolling', scrollUp);
+  return (
+    <div>
+      {
+        backToTop && (
+          <div className='scroll' onClick={scrollUp}>
+            ^
+          </div>
+      )}
+    </div>
+  )
+}
+
+export default ScrollToTop
