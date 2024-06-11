017f81d1f547f71fdedfb51b7f129498485b0bd6
Dominic <dominicezeifejafor@gmail.com>
2024-06-11 16:53:13 +0100

    changes

--- src/components/Third/Third.jsx
+++ src/components/Third/Third.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import TravelVlogCard from '../Cards/TravelVlogCard'
 import TravelData from '../../mock/travelVlogData'
+import { useLocation } from 'react-router-dom'
 import './Third.css'
 
 const Third = () => {
+  const location = useLocation();
+  const currentUrl = location.pathname;
   return (
     <div className='projectBody'>
+      <p>{currentUrl !== '/checkout' ? 'Shop' : 'Checkout'}</p>
       {
         TravelData.map(data => {
           return (
